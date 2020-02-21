30080c4bc96b304a29dca9a9d97905c9f3a4de03
Pablo <18240248@itleon.edu.mx>
2020-02-20 20:47:22 -0600

    Made homework Tarea 4

---
+++ src/CrazyButtons/Gui.java
@@ -0,0 +1,81 @@
+package CrazyButtons;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import javax.swing.JPanel;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Gui {
+    private JFrame    window;
+    private JPanel    mainPanel;
+    private JButton[] crazyButtons;
+
+    private final int WIDTH = 500, HEIGHT = 400;
+    private final int BTN_WIDTH = 105, BTN_HEIGHT = 25;
+
+    public Gui(int n) {
+        window       = new JFrame("Crazy Buttons");
+        mainPanel    = new JPanel();
+        crazyButtons = new JButton[n];
+        for (int i = 0; i < crazyButtons.length; i++)
+            crazyButtons[i] = new JButton("Click me!!");
+
+        this.atributos();
+        this.armado();
+        this.escuchas();
+        this.lanzar_GUI();
+    }
+
+    // Atributos de los componentes
+    public void atributos() {
+        window.setSize(WIDTH, HEIGHT);
+        window.setResizable(true);
+        mainPanel.setBackground(Color.PINK);
+        for (JButton btn : crazyButtons) {
+            int factor = (int) (Math.random() * 32) + 32;
+            btn.setBackground(new Color(factor * 4, factor * 3, factor * 3));
+        }
+    }
+
+    // Armar la interfaz
+    public void armado() {
+        window.add(mainPanel);
+        for (JButton btn : crazyButtons)
+            mainPanel.add(btn);
+    }
+
+    // Asignar los escuchas
+    public void escuchas() {
+        MouseAdapter controller = new Escucha();
+
+        for (JButton btn : crazyButtons)
+            btn.addMouseListener(controller);
+    }
+
+    // Lanzar la interfaz gráfica
+    public void lanzar_GUI() {
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+    private void positionate(JButton button) {
+        Random generator = new Random();
+        int x = generator.nextInt(WIDTH - BTN_WIDTH);
+        int y = generator.nextInt(HEIGHT - BTN_HEIGHT);
+
+        button.setBounds(x, y, BTN_WIDTH, BTN_HEIGHT);
+    }
+
+    public class Escucha extends MouseAdapter {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            for (JButton btn : crazyButtons)
+                if(e.getComponent().equals(btn))
+                    positionate(btn);
+        }
+    }
+}
---
+++ src/CrazyButtons/Prueba.java
@@ -0,0 +1,9 @@
+package CrazyButtons;
+
+import CrazyButtons.Gui;
+
+public class Prueba {
+    public static void main(String[] args) {
+        new Gui(10);
+    }
+}
