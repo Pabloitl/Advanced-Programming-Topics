078b6137a5acab0f57c46b2c33fc71d7bd868cf7
Pablo <18240248@itleon.edu.mx>
2020-02-20 21:03:31 -0600

    Sorted imports

--- src/CrazyButtons/Gui.java
+++ src/CrazyButtons/Gui.java
@@ -2,11 +2,11 @@ package CrazyButtons;
 
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
-import javax.swing.JPanel;
 import java.awt.event.MouseEvent;
 import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Gui {
     private JFrame    window;
