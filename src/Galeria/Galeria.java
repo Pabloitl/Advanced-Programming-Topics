21a3741bf24fce0e8e9a574d8dfac3e09fc4eb40
Pablo <18240248@itleon.edu.mx>
2020-02-22 17:16:21 -0600

    Added listeners

--- src/Galeria/Galeria.java
+++ src/Galeria/Galeria.java
@@ -3,9 +3,12 @@ package Galeria;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,10 +23,10 @@ public class Galeria {
 
     private final File imgSource;
     private final int WIDTH = 400, HEIGHT = 400;
-    private final String imageSource = "src/Galeria/Imagenes/";
+    private final String SOURCE = "src/Galeria/Imagenes/";
 
     public Galeria() {
-        imgSource      = new File(imageSource);
+        imgSource      = new File(SOURCE);
         window         = new JFrame();
         selectionPanel = new JPanel();
         imagePanel     = new JPanel();
@@ -40,23 +43,30 @@ public class Galeria {
 
     public void atributos() {
         String[] imagesList = imgSource.list();
+
         window.setSize(WIDTH, HEIGHT);
         window.setResizable(true);
         window.setLayout(new BorderLayout());
         selectionPanel.setLayout(new FlowLayout());
         for (int i = 0; i < images.length; i++)
-            images[i].setIcon(scaleImage(imageSource + imagesList[i], 100));
+            images[i].setIcon(scaleImage(SOURCE + imagesList[i], 100));
     }
 
     public void armado() {
         window.add(selectionPanel, BorderLayout.WEST);
-        // window.add(imagePanel, BorderLayout.EAST);
-        // imagePanel.add(bigImage);
+        window.add(imagePanel, BorderLayout.CENTER);
+        imagePanel.add(bigImage);
         for (JLabel img : images)
             selectionPanel.add(img);
     }
 
     public void escuchas() {
+        Escucha controller = new Escucha();
+
+        for (JLabel img : images) {
+            img.addMouseListener(controller);
+        }
+
     }
 
     public void lanzar_GUI() {
@@ -66,7 +76,10 @@ public class Galeria {
     }
 
     private ImageIcon scaleImage(String source, int max) {
-        ImageIcon original = new ImageIcon(source);
+        return scaleImage(new ImageIcon(source), max);
+    }
+
+    private ImageIcon scaleImage(ImageIcon original, int max) {
         float ratio = 9 / 16f;
         ImageIcon scaled   = new ImageIcon(
                 original.getImage().getScaledInstance(
@@ -74,5 +87,18 @@ public class Galeria {
                         (int) (max * ratio),
                         Image.SCALE_AREA_AVERAGING));
         return scaled;
+    }
+
+    class Escucha extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            for (int i = 0; i < images.length; i++) {
+                if (images[i].equals(e.getComponent())) {
+                    String imgRoute = SOURCE + imgSource.list()[i];
+                    bigImage.setIcon(scaleImage(imgRoute, window.getWidth() / 2));
+                }
+            }
+
+        }
     }
 }
