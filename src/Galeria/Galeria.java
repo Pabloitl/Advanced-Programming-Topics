6f904a392c5498c1422ee6d71bec351de78a9188
Pablo <18240248@itleon.edu.mx>
2020-02-26 20:51:42 -0600

    Used more components

--- src/Galeria/Galeria.java
+++ src/Galeria/Galeria.java
@@ -1,36 +1,44 @@
 package Galeria;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class Galeria {
-    private JFrame window;
-    private JPanel selectionPanel, imagePanel;
-    private JLabel bigImage;
-    private JLabel[] images;
+
+    private JFrame       window;
+    private JScrollPane  scrollPanel;
+    private JPanel       imagePanel, selectionPanel;
+    private JLabel       bigImage;
+    private JLabel[]     images;
 
     private final File imgSource;
-    private final int WIDTH = 400, HEIGHT = 400,
-            THUMBNAIL_SIZE = WIDTH / 4, BIG_IMAGE_SIZE = WIDTH * 3 / 4 - WIDTH / 8;
+    private final int WIDTH = 500, HEIGHT = 400,
+            THUMBNAIL_SIZE = WIDTH / 4,
+            BIG_IMAGE_SIZE = WIDTH * 3 / 4 - WIDTH / 8;
     private final String SOURCE = "src/Galeria/Imagenes/";
 
     public Galeria() {
         imgSource      = new File(SOURCE);
         window         = new JFrame();
         selectionPanel = new JPanel();
+        scrollPanel    = new JScrollPane(selectionPanel);
         imagePanel     = new JPanel();
         bigImage       = new JLabel();
         images         = new JLabel[imgSource.list().length];
-        for (int i = 0; i < images.length; i++)
+        for (int i = 0; i < images.length; i++) {
             images[i] = new JLabel();
+        }
 
         this.atributos();
         this.armado();
@@ -44,16 +52,21 @@ public class Galeria {
         window.setSize(WIDTH, HEIGHT);
         window.setResizable(true);
         window.setLayout(new BorderLayout());
-        selectionPanel.setLayout(new FlowLayout());
+        scrollPanel.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPanel.setPreferredSize(
+                new Dimension(THUMBNAIL_SIZE + WIDTH / 16, 0));
+        selectionPanel.setLayout(
+                new BoxLayout(selectionPanel, BoxLayout.Y_AXIS));
         imagePanel.setLayout(new BorderLayout());
-        bigImage.setIcon(scaleImage(SOURCE + imagesList[0], BIG_IMAGE_SIZE));
+        bigImage.setHorizontalAlignment(JLabel.CENTER);
         for (int i = 0; i < images.length; i++)
             images[i].setIcon(scaleImage(SOURCE + imagesList[i], THUMBNAIL_SIZE));
     }
 
     public void armado() {
-        window.add(selectionPanel, BorderLayout.CENTER);
-        window.add(imagePanel, BorderLayout.EAST);
+        window.add(scrollPanel, BorderLayout.WEST);
+        window.add(imagePanel, BorderLayout.CENTER);
         imagePanel.add(bigImage, BorderLayout.CENTER);
         for (JLabel img : images)
             selectionPanel.add(img);
@@ -62,10 +75,8 @@ public class Galeria {
     public void escuchas() {
         Escucha controller = new Escucha();
 
-        for (JLabel img : images) {
+        for (JLabel img : images)
             img.addMouseListener(controller);
-        }
-
     }
 
     public void lanzar_GUI() {
@@ -80,7 +91,7 @@ public class Galeria {
 
     private ImageIcon scaleImage(ImageIcon original, int max) {
         float ratio = original.getIconHeight() / (float) original.getIconWidth();
-        ImageIcon scaled   = new ImageIcon(
+        ImageIcon scaled = new ImageIcon(
                 original.getImage().getScaledInstance(
                         max,
                         (int) (max * ratio),
@@ -89,13 +100,19 @@ public class Galeria {
     }
 
     class Escucha extends MouseAdapter {
+
         @Override
-        public void mouseClicked(MouseEvent e) {
+        public void mouseEntered(MouseEvent e) {
             for (int i = 0; i < images.length; i++)
                 if (images[i].equals(e.getComponent())) {
                     String imgRoute = SOURCE + imgSource.list()[i];
                     bigImage.setIcon(scaleImage(imgRoute, BIG_IMAGE_SIZE));
                 }
+        }
+        
+        @Override
+        public void mouseExited(MouseEvent e) {
+            bigImage.setIcon(null);
         }
     }
 }
